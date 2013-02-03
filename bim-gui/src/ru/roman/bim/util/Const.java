998437bcaca7d7ebe27542c0e4a0ac899efadcde
Roman Churganov <curdes@gmail.com>
2013-02-03 17:03:30 +0400

    1.24

--- bim-gui/src/ru/roman/bim/util/Const.java
+++ bim-gui/src/ru/roman/bim/util/Const.java
@@ -10,10 +10,10 @@ import org.apache.commons.io.FileUtils;
 public interface Const {
 
     boolean DEV_MODE = false;
-    //String DEFAULT_ENDPOINT = "http://churganovroman.appspot.com/DataProvider";
-    //String DEFAULT_ENDPOINT_WSDL = "http://churganovroman.appspot.com/wsdl/DataProvider.wsdl";
-    String DEFAULT_ENDPOINT = "http://localhost:8080/DataProvider";
-    String DEFAULT_ENDPOINT_WSDL = "http://localhost:8080/wsdl/DataProvider.wsdl";
+    String DEFAULT_ENDPOINT = "http://churganovroman.appspot.com/DataProvider";
+    String DEFAULT_ENDPOINT_WSDL = "http://churganovroman.appspot.com/wsdl/DataProvider.wsdl";
+    //String DEFAULT_ENDPOINT = "http://localhost:8080/DataProvider";
+    //String DEFAULT_ENDPOINT_WSDL = "http://localhost:8080/wsdl/DataProvider.wsdl";
     String VERSION = "1.24";
 
 
